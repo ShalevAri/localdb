a8f4bac1ccbfcdca1de7d30423dc3636980adbac
shalev <shalevari2008@gmail.com>
2024-05-02 18:45:12 +0300

    feat: add global consts file

---
+++ consts.ts
@@ -0,0 +1,9 @@
+// Here declare constants that either don't or barely change.
+// This is so that you can easily change them in one place.
+// Simply import them in your code to use.
+
+// Local MySQL Connection
+export const LOCAL_MYSQL_DB_HOST = "localhost" // you may run into issues with the LOCAL_MYSQL_DB_HOST value set as "localhost" so if you do try changing it to "127.0.0.1" and see if that works.
+export const LOCAL_MYSQL_DB_USER = "root" // do not change!
+export const LOCAL_MYSQL_DB_PASSWORD = "your-local-mysql-password-here"
+export const LOCAL_MYSQL_DB_NAME = "localdb"
