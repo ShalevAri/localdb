d88286fb532a4299bc8af5ba2b46027fd42a052d
shalev <shalevari2008@gmail.com>
2024-05-06 18:32:27 +0300

    feat(app main-page): start main page

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,5 +1,12 @@
-import "./App.css"
+import "./styles/main-page/main-page__hero.scss"
 
 export default function App() {
-  return <main></main>
+  return (
+    <main>
+      <div className="hero">
+        <h1>LocalDB</h1>
+        <p>Interact with your local MySQL database effortlessly</p>
+      </div>
+    </main>
+  )
 }
