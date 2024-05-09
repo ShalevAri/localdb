e9d07f7a4ced059a547486cbee34d89cbe00e9a3
shalev <shalevari2008@gmail.com>
2024-05-09 18:51:51 +0300

    chore(consts): add warning

--- consts.ts
+++ consts.ts
@@ -5,5 +5,5 @@
 // Local MySQL Connection
 export const LOCAL_MYSQL_DB_HOST = "localhost" // you may run into issues with the LOCAL_MYSQL_DB_HOST value set as "localhost" so if you do try changing it to "127.0.0.1" and see if that works.
 export const LOCAL_MYSQL_DB_USER = "root" // do not change!
-export const LOCAL_MYSQL_DB_PASSWORD = "your-local-mysql-password-here"
+export const LOCAL_MYSQL_DB_PASSWORD = "your-local-mysql-password-here" // WARNING: make sure to never leave a space in the password value!
 export const LOCAL_MYSQL_DB = "localdb"
