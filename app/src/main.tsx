f0674c29092288853bb58bcf64b81b2d5c15cf82
shalev <shalevari2008@gmail.com>
2024-05-12 12:22:17 +0300

    feat(app): implement router

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -2,10 +2,18 @@ import "@/styles/globals/reset.scss"
 import "@/styles/globals/theme.scss"
 import React from "react"
 import ReactDOM from "react-dom/client"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import App from "./App.tsx"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
