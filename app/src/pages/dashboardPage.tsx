71b4211989e8c729ac16ca2ce12c85b98966d9fa
shalev <shalevari2008@gmail.com>
2024-05-16 18:38:35 +0300

    feat(app dashboard): add extra spacing

--- app/src/pages/dashboardPage.tsx
+++ app/src/pages/dashboardPage.tsx
@@ -5,6 +5,7 @@ export default function DashboardPage() {
     <main>
       <div className="top">
         <h1 className="top__title">LocalDB</h1>
+        <div className="top__spacer-one"></div>
         <div className="top__buttons-container">
           <div className="top__buttons-container__button top__buttons-container__button--left">
             <button className="top__buttons-container__button-element top__buttons-container__button-element--left">
