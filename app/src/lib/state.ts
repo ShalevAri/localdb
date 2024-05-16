bcc8c5cea7d75a9ce04d0e48e8c506856a12adbc
shalev <shalevari2008@gmail.com>
2024-05-16 19:41:31 +0300

    fix(app state): use local storage

--- app/src/lib/state.ts
+++ app/src/lib/state.ts
@@ -1,9 +1,9 @@
 // this is a global state file
 // Here I export the Jotai Atoms
 
-import { atom } from "jotai"
+import { atomWithStorage } from "jotai/utils"
 
-export const isLoggedInAtom = atom(false)
-export const usernameAtom = atom("")
-export const emailAtom = atom("")
-export const passwordAtom = atom("")
+export const isLoggedInAtom = atomWithStorage<boolean>("isLoggedIn", false)
+export const usernameAtom = atomWithStorage<string>("username", "")
+export const emailAtom = atomWithStorage<string>("email", "")
+export const passwordAtom = atomWithStorage<string>("password", "")
